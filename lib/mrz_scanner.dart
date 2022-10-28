acc245cc424fd0c221bbbf9aca419236793de635
F-BONAPARTA <67749770+F-BONAPARTA@users.noreply.github.com>
2022-10-28 20:13:10 +0200

    commit

--- lib/mrz_scanner.dart
+++ lib/mrz_scanner.dart
@@ -3,4 +3,3 @@ library mrz_scanner;
 export 'src/mrz_scanner.dart';
 export 'src/mrz_controller.dart';
 export 'package:mrz_parser/mrz_parser.dart';
-export 'package:camera_platform_interface/src/types/camera_description.dart';
