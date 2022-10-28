78009cc53bb3cd8fd33cc69448a84f6acd40a0c3
F-BONAPARTA <67749770+F-BONAPARTA@users.noreply.github.com>
2022-10-28 20:14:58 +0200

    commit

--- lib/mrz_scanner.dart
+++ lib/mrz_scanner.dart
@@ -3,3 +3,4 @@ library mrz_scanner;
 export 'src/mrz_scanner.dart';
 export 'src/mrz_controller.dart';
 export 'package:mrz_parser/mrz_parser.dart';
+export 'package:camera_platform_interface/src/types/camera_description.dart';
--- lib/src/mrz_scanner.dart
+++ lib/src/mrz_scanner.dart
@@ -8,9 +8,11 @@ class MRZScanner extends StatefulWidget {
   const MRZScanner({
     Key? controller,
     required this.onSuccess,
+    this.initialDirection = CameraLensDirection.back,
     this.showOverlay = true,
   }) : super(key: controller);
   final Function(MRZResult mrzResult) onSuccess;
+  final CameraLensDirection initialDirection;
   final bool showOverlay;
   @override
   // ignore: library_private_types_in_public_api
@@ -35,6 +37,7 @@ class MRZScannerState extends State<MRZScanner> {
   Widget build(BuildContext context) {
     return MRZCameraView(
       showOverlay: widget.showOverlay,
+      initialDirection: widget.initialDirection,
       onImage: _processImage,
     );
   }
