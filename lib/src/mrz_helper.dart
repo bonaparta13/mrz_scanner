5cf368ba98ed65e9f214ba03eb84ca772efda802
Juan Jose Valencia <juanjv1016@gmail.com>
2024-01-17 08:19:41 -0500

    refactor code

--- lib/src/mrz_helper.dart
+++ lib/src/mrz_helper.dart
@@ -14,7 +14,6 @@ class MRZHelper {
     List<String> firstLineChars = ableToScanTextList.first.split('');
     List<String> supportedDocTypes = ['A', 'C', 'P', 'V', 'I'];
     String fChar = firstLineChars[0];
-    String sChar = firstLineChars[1];
     if (supportedDocTypes.contains(fChar)) {
       return [...ableToScanTextList];
     }
--- lib/src/mrz_scanner.dart
+++ lib/src/mrz_scanner.dart
@@ -54,8 +54,6 @@ class MRZScannerState extends State<MRZScanner> {
     }
   }
 
-  List results = [];
-
   Future<void> _processImage(InputImage inputImage) async {
     if (!_canProcess) return;
     if (_isBusy) return;
@@ -73,37 +71,6 @@ class MRZScannerState extends State<MRZScanner> {
       }
     }
     List<String>? result = MRZHelper.getFinalListToParse([...ableToScanText]);
-
-    // if (result != null) {
-    //   if (results.length == 3 &&
-    //       results[2] == results[1] &&
-    //       results[2] == results[0]) {
-    //     _parseScannedText([...result]);
-    //     return;
-    //   }
-
-    //   results.add(result.join());
-    //   if (results.length > 3) {
-    //     results.removeAt(0);
-    //   }
-
-    //   print(results);
-    //   _isBusy = false;
-    //   // if (results.length == 3 &&
-    //   //     result[0] == results.first &&
-    //   //     result[0] == results[1]) {
-    //   //   _isBusy = false;
-    //   //   print('success$results');
-    //   // } else if (results.length > 3) {
-    //   //   print('remove first');
-    //   //   print('results$results');
-    //   //   results.remove(results[0]);
-    //   //   _isBusy = false;
-    //   // }
-    // } else {
-    //   print('result null');
-    //   _isBusy = false;
-    // }
 
     if (result != null) {
       _parseScannedText([...result]);
